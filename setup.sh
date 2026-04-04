388d4fba1f6e4cce103cae8bc8c12bef96831704
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-04-04 16:19:17 +0000

    m

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
+if test -d /content/; then
+    CONTENT=/content
+else
+    CONTENT=$HOME
+fi
 sudo apt install git tesseract-ocr tesseract-ocr-ron poppler-utils
-if ! test -d /content/pdfutils; then
+if ! test -d $CONTENT/pdfutils; then
     cd
     git clone https://github.com/Danielzapirtan/pdfutils
 fi
 
-cd /content/pdfutils
+cd $CONTENT/pdfutils
 echo "Alegeti aplicatia"
 echo "pdarg isplit ocr_pdf pdf_splitter"
 read ans
-cd /content/pdfutils/$ans
+cd $CONTENT/pdfutils/$ans
 pip install -r requirements.txt
 python3 app.py
