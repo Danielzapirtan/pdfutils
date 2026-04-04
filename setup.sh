b93ac2267b0e8240603462b65fd4ebd18936e18a
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-04-04 16:28:12 +0000

    m

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ if test -d /content/; then
 else
     CONTENT=$HOME
 fi
+sudo apt update
 sudo apt install git tesseract-ocr tesseract-ocr-ron poppler-utils -y
 if ! test -d $CONTENT/pdfutils; then
     cd
